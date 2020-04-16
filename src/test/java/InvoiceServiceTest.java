440160cceaf4e23775a68919ac1126459b5e9082
kirtibhave <kirtibhave1000@gmail.com>
2020-04-16 16:46:38 +0530

    ADDED:Enhanced Invoice Generator to return expected part

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -6,34 +6,33 @@ public class InvoiceServiceTest {
     InvoiceGenerator invoiceGenerator = null;
 
     @Before
-    public void setUp() throws Exception{
+    public void setUp() throws Exception {
         invoiceGenerator = new InvoiceGenerator();
     }
 
     @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare(){
-       double distance = 2.0;
-       int time = 5;
-       double fare = invoiceGenerator.calculateFare(distance, time);
-       Assert.assertEquals(25,fare,0.0);
-   }
-
-   @Test
-    public void givenLessDistanceOrTime_ShouldReturnMinimumFare(){
-       double distance = 0.1;
-       int time = 1;
-       double fare = invoiceGenerator.calculateFare(distance, time);
-       Assert.assertEquals(5,fare,0.0);
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
     }
 
     @Test
-    public void givenMultipleRides_ShouldReturnTotalFare(){
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare() {
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
         Ride[] rides = {new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
-
-     double fare = invoiceGenerator.calculateFare(rides);
-     Assert.assertEquals(30,fare,0.0);
-
+        InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedInvoiceSummary, summary);
     }
 }
