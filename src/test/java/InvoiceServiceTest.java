36caea82cf7fd711a679213af88bce94b1924b46
kirtibhave <kirtibhave1000@gmail.com>
2020-04-16 10:53:11 +0530

    Calculate aggregate total for all rides by taking multiple rides

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -1,10 +1,17 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+    InvoiceGenerator invoiceGenerator = null;
+
+    @Before
+    public void setUp() throws Exception{
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare(){
-       InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
        double distance = 2.0;
        int time = 5;
        double fare = invoiceGenerator.calculateFare(distance, time);
@@ -13,10 +20,20 @@ public class InvoiceServiceTest {
 
    @Test
     public void givenLessDistanceOrTime_ShouldReturnMinimumFare(){
-       InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
        double distance = 0.1;
        int time = 1;
        double fare = invoiceGenerator.calculateFare(distance, time);
        Assert.assertEquals(5,fare,0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare(){
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+
+     double fare = invoiceGenerator.calculateFare(rides);
+     Assert.assertEquals(30,fare,0.0);
+
     }
 }
